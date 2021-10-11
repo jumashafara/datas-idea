9b6f5b3758dd237ad6942ee7595909c63665189b
Kibekityo Juma Shafara <79038078+shafara33@users.noreply.github.com>
2021-10-11 08:16:56 -0700

    ready to deploy

--- src/Apis/blogs_api.js
+++ src/Apis/blogs_api.js
@@ -1,6 +1,6 @@
-export const getBlogs = () => fetch("http://localhost:5550/blogs").then(res => res.json())
+export const getBlogs = () => fetch("https://datas-idea.herokuapp.com/blogs").then(res => res.json())
 
-export const addBlog = (blog) => fetch("http://localhost:5550/blogs/add-blog", {
+export const addBlog = (blog) => fetch("https://datas-idea.herokuapp.com/blogs/add-blog", {
     method: "POST",
     headers: {
         "Accept": "application/json",
@@ -9,4 +9,4 @@ export const addBlog = (blog) => fetch("http://localhost:5550/blogs/add-blog", {
     body: JSON.stringify(blog)
 })
 
-export const getBlog = (id) => fetch(`http://localhost:5550/blogs/${id}`).then(res => res.json())
+export const getBlog = (id) => fetch(`https://datas-idea.herokuapp.com/blogs/${id}`).then(res => res.json())
--- src/Apis/comments_api.js
+++ src/Apis/comments_api.js
@@ -1,6 +1,6 @@
-export const getComments = () => fetch("http://localhost:5550/comments").then(res => res.json())
+export const getComments = () => fetch("https://datas-idea.herokuapp.com/comments").then(res => res.json())
 
-export const addComment = (comment) => fetch("http://localhost:5550/comments/add-comment", {
+export const addComment = (comment) => fetch("https://datas-idea.herokuapp.com/comments/add-comment", {
     method: "POST",
     headers: {
         "Accept": "application/json",
